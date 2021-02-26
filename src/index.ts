1b1d94b8e78a5c3b9a0da46da954570d8aea0583
Rafaela-Quinzel <71155947+Rafaela-Quinzel@users.noreply.github.com>
2021-02-26 12:27:48 -0300

    Fiz um pequeno ajuste no index.ts

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ app.use("/music", musicRouter)
 
 
 
-const server = app.listen(3000, () => {
+const server = app.listen(3003, () => {
    if (server) {
       const address = server.address() as AddressInfo;
       console.log(`Servidor rodando em http://localhost:${address.port}`)
